131d7201e404b767ed6dea02ae1891c43f15014b
moscatellirafaell <89915262+moscatellirafaell@users.noreply.github.com>
2022-09-27 16:58:14 -0300

    finished

--- Music Player/music_player.py
+++ Music Player/music_player.py
@@ -2,8 +2,10 @@ import PySimpleGUI as sg
 import base64
 from io import BytesIO
 from PIL import Image
-from pygame import mixer
+from pygame import mixer, time
+
 mixer.init()
+clock = time.Clock()
 
 
 def base64_image_import(path):
@@ -18,7 +20,12 @@ def base64_image_import(path):
 path = sg.popup_get_file('Open', no_window=True)
 song_name = path.split('/')[-1].split('.')[0]
 song = mixer.Sound(path)
-song.play()
+
+# timer
+song_length = int(song.get_length())
+time_since_start = 0
+pause_amount = 0
+playing = False
 
 sg.theme('reddit')
 
@@ -34,7 +41,7 @@ play_layout = [
                button_color='white', border_width=0),
      sg.Push()],
     [sg.VPush()],
-    [sg.Progress(100, size=(20, 20), key='-PROGRESS-')]
+    [sg.Progress(song_length, size=(20, 20), key='-PROGRESS-')]
 ]
 
 volume_layout = [
@@ -51,8 +58,26 @@ layout = [
 window = sg.Window('Music Player', layout)
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=1)
     if event == sg.WIN_CLOSED:
         break
+
+    if playing:
+        time_since_start = time.get_ticks()
+        window['-PROGRESS-'].update((time_since_start - pause_amount) // 1000)
+
+    if event == '-PLAY-':
+        playing = True
+        pause_amount += time.get_ticks() - time_since_start
+        if mixer.get_busy() == False:
+            song.play()
+        else:
+            mixer.unpause()
+
+    if event == '-PAUSE-':
+        playing = False
+        mixer.pause()
+
+    song.set_volume(values['-VOLUME-'] / 100)
 
 window.close()
